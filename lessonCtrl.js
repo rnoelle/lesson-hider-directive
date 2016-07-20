8e9dc6e62c7282bc3bb48bba5196332debbd337a
rnoelle <reidnoelle2@gmail.com>
2016-07-20 08:45:24 -0600

    finished all options

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,4 +1,16 @@
 angular.module('directivePractice')
   .controller('lessonCtrl', function ($scope) {
+    // $scope.test = 'Two-way data binding!';
       $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+      $scope.crossed = false;
+
+      $scope.announceDay = function (lesson, day) {
+        console.log(day);
+        alert(lesson + ' is active on ' + day);
+      };
+
+      $scope.removeLesson = function (thisLesson) {
+        console.log("ran");
+          $scope.lessons.splice($scope.lessons.indexOf(thisLesson), 1);
+      };
   });
--- lessonHider.js
+++ lessonHider.js
@@ -1,9 +1,54 @@
 angular.module('directivePractice')
-  .directive('lessonHider', function () {
-    return {
-      restrict: 'E',
-      templateUrl: './lesson.html',
-      link: function (scope, element, attr) {
-      }
-    };
-  });
+    .directive('lessonHider', function() {
+        return {
+            templateUrl: './lesson.html',
+            restrict: 'E',
+            scope: {
+                lesson: '=',
+                dayAlert: '&',
+                crossed: '=crossed',
+                words: '=words',
+                deleteLesson: '&'
+            },
+            link: function(scope, element, attr) {
+              scope.getSchedule.then(function(response) {
+                  scope.schedule = response.data;
+                  scope.schedule.forEach(function(scheduleDay) {
+                      if (scheduleDay.lesson === scope.lesson) {
+                        scope.lessonDay = scheduleDay.weekday;
+                          if (scope.crossed) {
+                            element.css('text-decoration', 'line-through');
+                          } else {
+                            element.css('text-decoration', 'none');
+                          } return;
+                      }
+                  });
+                  if (scope.lessonDay === undefined) {
+                    scope.lessonDay = "on an unplanned day.";
+                  }
+              });
+                scope.$watch('crossed', function() {
+                  scope.getSchedule.then(function(response) {
+                      scope.schedule = response.data;
+                      scope.schedule.forEach(function(scheduleDay) {
+                          if (scheduleDay.lesson === scope.lesson) {
+                            scope.lessonDay = scheduleDay.weekday;
+                              if (scope.crossed) {
+                                element.css('text-decoration', 'line-through');
+                              } else {
+                                element.css('text-decoration', 'none');
+                              } return;
+                          }
+                      });
+                      if (scope.lessonDay === undefined) {
+                        scope.lessonDay = "on an unplanned day.";
+                      }
+                  });
+                });
+            },
+
+            controller: function($scope, lessonService) {
+                $scope.getSchedule = lessonService.getSchedule();
+            }
+        };
+    });
